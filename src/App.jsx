9c453c544c563bd930a55c89d142e189b383523e
Prashant <pygold@gmail.com>
2023-04-06 21:42:14 +0530

    completed redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import ReducerBasics from './Reducer/useReducer'
 import Navbar from './toolkit/components/Navbar'
 import CartContainer from './toolkit/components/CartContainer'
 import { useDispatch, useSelector } from 'react-redux'
-import { calculateTotals } from './toolkit/features/cart/cartSlice'
+import { calculateTotals, getCartItems } from './toolkit/features/cart/cartSlice'
 import Modal from './toolkit/components/Modal'
 
 
 function App() {
   // const [count, setCount] = useState(0);
-  const {cartItems} = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   console.log('here');
   console.log(cartItems);
@@ -22,33 +22,21 @@ function App() {
     dispatch(calculateTotals());
   }, [cartItems]);
 
+  useEffect(() => {
+    dispatch(getCartItems('We can pass params also'));
+  },[]);
+
+  if(isLoading) {
+    return (
+      <h1 className='loading'>Loading...</h1>
+    )
+  }
+
   return (
     <div>
       { isOpen && <Modal /> }
       <Navbar />
       <CartContainer />
-
-      {/* <div>
-        <ReducerBasics></ReducerBasics>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR. React is beautiful.
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </div>
   )
 }
--- src/toolkit/features/cart/cartSlice.js
+++ src/toolkit/features/cart/cartSlice.js
@@ -1,12 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { openModal } from "../modal/modalSlice";
+
+const url = 'https://course-api.com/react-useReducer-cart-project';
+const errorUrl = 'https://course-api.com/react-useReducer-cart-projectsss';
 
 const initialState = {
-    cartItems: cartItems,
+    cartItems: [],
     amount: 1,
     total: 0,
     isLoading: true
 }
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', async (weCanPassParamsHere, thunkAPI) => {
+    // return fetch(url)
+    // .then( response => response.json())
+    // .catch(err => err);
+
+    // Using axios
+    try {
+        console.log(weCanPassParamsHere);
+        console.log(thunkAPI);
+        console.log(thunkAPI.getState()); 
+    
+        // We can dispatch action from here thunkApi
+        // thunkAPI.dispatch(openModal());
+
+        const response = await axios(url);
+        console.log(response);
+        return response.data;
+    } catch(err) {
+        // To induce this error pass errorUrl to axios
+        return thunkAPI.rejectWithValue('We are inducing this error');
+    }
+});
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -36,6 +63,20 @@ const cartSlice = createSlice({
             });
             state.amount = amount;
             state.total = total;
+        },
+    },
+    extraReducers: {
+        [getCartItems.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [getCartItems.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.cartItems = action.payload;
+            console.log(action);
+        },
+        [getCartItems.rejected]: (state, action) => {
+            state.isLoading = false;
+            console.log(action);
         }
     }
 })
