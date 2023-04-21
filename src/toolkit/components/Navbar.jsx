c86f9f7c9ec61945752aeba746bea74c22e12f46
Prashant Yadav <pygold@gmail.com>
2023-04-21 20:06:40 +0000

    Changed redux to Redux

--- src/toolkit/components/Navbar.jsx
+++ src/toolkit/components/Navbar.jsx
@@ -3,24 +3,36 @@ import { useNavigate } from "react-router-dom";
 import { CartIcon, Testing } from "../icons";
 
 const Navbar = () => {
-    const navigate = useNavigate();
-    // console.log(useSelector((store)=>{console.log(store)}));
-    // const amount = useSelector((store) => store.cart.amount);
-    const { amount } = useSelector((store) => store.cart);
-    return (
-        <nav>
-            <div className="nav-center">
-                <a onClick={() => {navigate('/')}}>redux toolkit</a>
-                <a onClick={() => {navigate('/pokemon')}}>Pokemon</a>
-                <div className="nav-container">
-                    <CartIcon />
-                    <div className="amount-container">
-                        <div className="total-amount">{amount}</div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
-}
+  const navigate = useNavigate();
+  // console.log(useSelector((store)=>{console.log(store)}));
+  // const amount = useSelector((store) => store.cart.amount);
+  const { amount } = useSelector((store) => store.cart);
+  return (
+    <nav>
+      <div className="nav-center">
+        <a
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Redux toolkit
+        </a>
+        <a
+          onClick={() => {
+            navigate("/pokemon");
+          }}
+        >
+          Pokemon
+        </a>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <div className="total-amount">{amount}</div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
